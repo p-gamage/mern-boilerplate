ad1f54e39b6e751c4057c97680373d193e386c13
Priyanga Gamage <pgamag3@gmail.com>
2018-06-04 18:00:52 +0100

    correct webpack configuration

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+const publicPath = path.resolve(__dirname, '..', '..', 'public');
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
+app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-  console.log('application is listening from port 3000');
+  console.log(`application is listening from port 3000 and looking in dir ${publicPath}`);
 });
